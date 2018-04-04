b5bd129586adcdcb90dad7c87e6ae4e00641177f
Alexandra Leonova <kabrenok@gmail.com>
2018-04-04 23:35:16 +0300

    Модули и переключение. Не работает возврат на приветствие после окончания игры

--- js/main.js
+++ js/main.js
@@ -1,41 +1,20 @@
-let screens = document.getElementById(`templates`).content.children;
-const mainSectionNode = document.querySelector(`section.main`);
+import {showScreen} from './show-screen.js';
+import {moduleWelcome} from './template-welcome.js';
 
-let screensNodes = Array.from(screens);
+showScreen(moduleWelcome);
+// export const renderMain = () => {
+//   // const screenNode = getElement(moduleWelcomeTemplate).cloneNode();
+//   createScreenNode(moduleWelcomeTemplate);
+//   const buttonPlay = document.querySelector(`.main-play`);
+//   buttonPlay.addEventListener(`click`, () => {
+//     createScreenNode(moduleArtistTemplate);
+//   });
+// };
+// renderMain();
 
-const showScreen = (screenNumber) => {
-  mainSectionNode.appendChild(screensNodes[screenNumber]);
-};
-const hideScreen = (screenNumber) => {
-  mainSectionNode.removeChild(screensNodes[screenNumber]);
-};
+// createScreenNode(moduleWelcomeTemplate);
+// const buttonPlay = document.querySelector(`.main-play`);
+// buttonPlay.addEventListener(`click`, () => {
+//   createScreenNode(moduleArtistTemplate);
+// });
 
-let index = 0;
-showScreen(index);
-
-document.addEventListener(`keydown`, (evt) => {
-  if (evt.altKey && evt.key === `ArrowRight`) {
-    if (index < screensNodes.length - 1) {
-      hideScreen(index);
-      showScreen(index + 1);
-      index++;
-    } else {
-      index = screensNodes.length - 1;
-    }
-  }
-  if (evt.altKey && evt.key === `ArrowLeft`) {
-    if (index > 0) {
-      hideScreen(index);
-      showScreen(index - 1);
-      index--;
-    } else {
-      index = 0;
-    }
-  }
-
-  // Подождать озарения
-  // const getNextScreenNumber = (count, current) => {
-  //   const next = current % count;
-  //   return next + (next < 0 ? count : 0);
-  // };
-});
