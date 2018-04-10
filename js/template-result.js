817dc8eee73b3df044d8534a353843b3f6d6f11b
Alexandra Leonova <kabrenok@gmail.com>
2018-04-10 17:26:21 +0300

    Клоны нод, шаблоны вынесены из экспорта

--- js/template-result.js
+++ js/template-result.js
@@ -1,48 +1,50 @@
 import {getElement, showScreen} from './show-screen.js';
 import {moduleWelcome} from './template-welcome.js';
 
+const templateResultVictory = getElement(`<section class="main main--result">
+  <section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>
+
+  <h2 class="title">Вы настоящий меломан!</h2>
+  <div class="main-stat">За&nbsp;3&nbsp;минуты и 25&nbsp;секунд
+    <br>вы&nbsp;набрали 12 баллов (8 быстрых)
+    <br>совершив 3 ошибки</div>
+  <span class="main-comparison">Вы заняли 2 место из 10. Это&nbsp;лучше чем у&nbsp;80%&nbsp;игроков</span>
+  <span role="button" tabindex="0" class="main-replay">Сыграть ещё раз</span>
+</section>`);
 export const moduleResultVictory = function () {
-  const templateResultVictory = getElement(`<section class="main main--result">
-    <section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>
-
-    <h2 class="title">Вы настоящий меломан!</h2>
-    <div class="main-stat">За&nbsp;3&nbsp;минуты и 25&nbsp;секунд
-      <br>вы&nbsp;набрали 12 баллов (8 быстрых)
-      <br>совершив 3 ошибки</div>
-    <span class="main-comparison">Вы заняли 2 место из 10. Это&nbsp;лучше чем у&nbsp;80%&nbsp;игроков</span>
-    <span role="button" tabindex="0" class="main-replay">Сыграть ещё раз</span>
-  </section>`);
-  const againButton = templateResultVictory.querySelector(`.main-replay`);
+  const cloneVictory = templateResultVictory.cloneNode(true);
+  const againButton = cloneVictory.querySelector(`.main-replay`);
   againButton.addEventListener(`click`, () => showScreen(moduleWelcome()));
-
-  return templateResultVictory;
+  return cloneVictory;
 };
 
+const templateResultTime = getElement(`<section class="main main--result">
+  <section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>
+
+  <h2 class="title">Увы и ах!</h2>
+  <div class="main-stat">Время вышло!<br>Вы не успели отгадать все мелодии</div>
+  <span role="button" tabindex="0" class="main-replay">Попробовать ещё раз</span>
+</section>`);
 export const moduleResultTime = function () {
-  const templateResultTime = getElement(`<section class="main main--result">
-    <section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>
-
-    <h2 class="title">Увы и ах!</h2>
-    <div class="main-stat">Время вышло!<br>Вы не успели отгадать все мелодии</div>
-    <span role="button" tabindex="0" class="main-replay">Попробовать ещё раз</span>
-  </section>`);
-  const againButton = templateResultTime.querySelector(`.main-replay`);
+  const cloneTime = templateResultTime.cloneNode(true);
+  const againButton = cloneTime.querySelector(`.main-replay`);
   againButton.addEventListener(`click`, () => showScreen(moduleWelcome()));
 
-  return templateResultTime;
+  return cloneTime;
 };
 
-export const moduleResultAttempts = function () {
-  const templateResultAttempts = getElement(`<section class="main main--result">
-    <section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>
+const templateResultAttempts = getElement(`<section class="main main--result">
+  <section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>
 
-    <h2 class="title">Какая жалость!</h2>
-    <div class="main-stat">У вас закончились все попытки.<br>Ничего, повезёт в следующий раз!</div>
-    <span role="button" tabindex="0" class="main-replay">Попробовать ещё раз</span>
-  </section>`);
-  const againButton = templateResultAttempts.querySelector(`.main-replay`);
+  <h2 class="title">Какая жалость!</h2>
+  <div class="main-stat">У вас закончились все попытки.<br>Ничего, повезёт в следующий раз!</div>
+  <span role="button" tabindex="0" class="main-replay">Попробовать ещё раз</span>
+</section>`);
+export const moduleResultAttempts = function () {
+  const cloneAttempts = templateResultAttempts.cloneNode(true);
+  const againButton = cloneAttempts.querySelector(`.main-replay`);
   againButton.addEventListener(`click`, () => showScreen(moduleWelcome()));
-  return templateResultAttempts;
+  return cloneAttempts;
 };
 
 
--- js/template-welcome.js
+++ js/template-welcome.js
@@ -1,19 +1,19 @@
 import {getElement, showScreen} from './show-screen.js';
 import {moduleArtists} from './template-artists.js';
 
+const templateWelcome = getElement(`<section class="main main--welcome">
+  <section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>
+  <button class="main-play">Начать игру</button>
+  <h2 class="title main-title">Правила игры</h2>
+  <p class="text main-text">
+    Правила просты&nbsp;— за&nbsp;5 минут ответить на все вопросы.<br>
+    Ошибиться можно 3 раза.<br>
+    Удачи!
+  </p>
+</section>`);
 export const moduleWelcome = function () {
-  const templateWelcome = getElement(`<section class="main main--welcome">
-    <section class="logo" title="Угадай мелодию"><h1>Угадай мелодию</h1></section>
-    <button class="main-play">Начать игру</button>
-    <h2 class="title main-title">Правила игры</h2>
-    <p class="text main-text">
-      Правила просты&nbsp;— за&nbsp;5 минут ответить на все вопросы.<br>
-      Ошибиться можно 3 раза.<br>
-      Удачи!
-    </p>
-  </section>`);
-
-  const buttonPlay = templateWelcome.querySelector(`.main-play`);
+  const cloneWelcome = templateWelcome.cloneNode(true);
+  const buttonPlay = cloneWelcome.querySelector(`.main-play`);
   buttonPlay.addEventListener(`click`, () => showScreen(moduleArtists()));
-  return templateWelcome;
+  return cloneWelcome;
 };
